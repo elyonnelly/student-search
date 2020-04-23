2f3a09003f91b75f1341c828f26b6d5c1612e793
elyonnelly <alfedorova.ru@gmail.com>
2020-04-23 22:52:20 +0500

    do refactoring

--- server/src/main/java/StudentSearchClient.java
+++ server/src/main/java/StudentSearchClient.java
@@ -19,23 +19,18 @@ import java.util.List;
 public class StudentSearchClient {
     VkAppSettings appSettings;
     VkApiClient vkClient;
-    String userCode;
     HttpRequestListener requestListener;
-    UserActor userActor;
-    GroupActor groupActor;
     HttpServer server;
-    List<Integer> groups;
+
+    StudentSearchUserClient user;
+    StudentSearchGroupClient group;
 
     public StudentSearchClient() {
         appSettings = new VkAppSettings();
         TransportClient transportClient = HttpTransportClient.getInstance();
         vkClient = new VkApiClient(transportClient);
         requestListener = new HttpRequestListener();
-    }
-
-    public void doAuthorization() throws ClientException, ApiException, URISyntaxException, IOException, HttpException, InterruptedException {
-        setAccessToken();
-        setUserActor();
+        user = new StudentSearchUserClient(this);
     }
 
     public void startApp() {
@@ -50,56 +45,6 @@ public class StudentSearchClient {
         }
     }
 
-    public void overviewGroups() throws ClientException, ApiException {
-        groups = vkClient.groups().get(userActor).filter(GroupsGetFilter.ADMIN).execute().getItems();
-    }
-
-    private void setAccessToken() throws HttpException, IOException, InterruptedException, URISyntaxException, ClientException, ApiException {
-        getUserCode();
-
-        UserAuthResponse authResponse = vkClient.oauth()
-                .userAuthorizationCodeFlow(Integer.valueOf(appSettings.app_id), appSettings.client_secret, appSettings.redirect_uri, userCode)
-                .execute();
-        appSettings.access_token = authResponse.getAccessToken();
-        appSettings.user_id = authResponse.getUserId();
-    }
-
-    public void setGroupActor() {
-        GroupActor actor = new GroupActor(appSettings.group_id, appSettings.access_token);
-    }
-
-    public void setUserActor() throws ClientException, ApiException, URISyntaxException, IOException, HttpException, InterruptedException {
-        userActor = new UserActor(appSettings.user_id, appSettings.access_token);
-    }
-
-    private void getUserCode() throws URISyntaxException, IOException, HttpException, InterruptedException {
-        Desktop.getDesktop().browse(getAuthorizationUri());
-
-        //здесь нужно врубить ожидание ответа от сервера крч
-        while(requestListener.parameters == null) {
-             Thread.sleep(2000);
-        }
-        System.out.println("success");
-        var parameter = requestListener.parameters.get(0);
-        if (parameter.getName().equals("code")) {
-            userCode = parameter.getValue();
-        }
-        else {
-            throw new HttpException("code parameter not received");
-        }
-    }
-
-    private URI getAuthorizationUri() throws URISyntaxException {
-        String authorizationUri = "https://oauth.vk.com/authorize";
-        URIBuilder uri = new URIBuilder(authorizationUri);
-        uri.addParameter("client_id", appSettings.app_id);
-        uri.addParameter("display", "page");
-        uri.addParameter("redirect_uri", appSettings.redirect_uri);
-        uri.addParameter("scope", "groups");
-        uri.addParameter("response_type", "code");
-        uri.addParameter("v", "5.45");
-        return uri.build();
-    }
 }
 
 
--- server/src/test/java/VKClientTest.java
+++ server/src/test/java/VKClientTest.java
@@ -15,19 +15,16 @@ class VKClientTest {
     void testStartUp() throws IOException, URISyntaxException, HttpException, ApiException, ClientException, InterruptedException {
         StudentSearchClient client = new StudentSearchClient();
         client.startApp();
-        client.doAuthorization();
-        assertNotNull(client.userCode);
-        assertNotNull(client.userActor);
+        client.user.doAuthorization();
+        assertNotNull(client.user.userActor);
     }
 
     @Test
     void testGetGroups() throws InterruptedException, HttpException, IOException, ApiException, URISyntaxException, ClientException {
         StudentSearchClient client = new StudentSearchClient();
         client.startApp();
-        client.setUserActor();
-        client.overviewGroups();
-        client.setGroupActor();
-        assertNotNull(client.groupActor);
-        assertEquals(client.appSettings.group_id, client.groups.get(0));
+        client.user.setUserActor();
+        client.user.overviewGroups();
+        assertEquals(client.appSettings.group_id, client.user.groups.get(0));
     }
 }

6a7bf1a6f58ad16825981a58757d1091db17109c
elyonnelly <alfedorova.ru@gmail.com>
2020-04-24 20:57:17 +0500

    add sketch of GUI

--- client/src/main/java/Controller.java
+++ client/src/main/java/Controller.java
@@ -1,11 +1,6 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 public class Controller {
 
     @FXML
@@ -13,11 +8,6 @@ public class Controller {
 
     @FXML
     void onActionAuthorization() {
-        Runtime runtime = Runtime.getRuntime();
-        try {
-            Desktop.getDesktop().browse(new URI("https://vk.com"));
-        } catch (IOException | URISyntaxException e) {
-            e.printStackTrace();
-        }
+
     }
 }
